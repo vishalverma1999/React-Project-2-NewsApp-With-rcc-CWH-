a01c9f63ce73e67df4554227cda1466aacf23cd1
vishalverma1999 <vermavishal1999.vv@gmail.com>
2021-10-20 02:32:29 +0530

    checking if remote added correctly

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        Hello my first class based Component {this.c}
+        Hello my first class based Component {this.c} checking if remote added correctly
       </div>
     )
   }
