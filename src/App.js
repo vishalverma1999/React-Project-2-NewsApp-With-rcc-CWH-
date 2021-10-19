2f68eab9b43967e7422a84eb929c4ff537b7ed4f
vishalverma1999 <vermavishal1999.vv@gmail.com>
2021-10-20 02:22:21 +0530

    #22 Project 2 Setup + Introduction to Class based components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+ c = "john"
+  render() {
+    return (
+      <div>
+        Hello my first class based Component {this.c}
+      </div>
+    )
+  }
+}
