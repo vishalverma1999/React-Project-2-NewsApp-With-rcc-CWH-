21831a6aece7b815869cb6cb2728deea751174f0
vishalverma1999 <vermavishal1999.vv@gmail.com>
2021-10-20 20:14:09 +0530

    #24 Fetching API Key from News API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
+        let {title, description } = this.props;   // We are using destructuring concept...props mein hum object denge aur us object se title aur description waali keys ko pull karlo 
         return (
             <div>
-                This is a newsItemComponent2
+                <div className="card" style={{width: "18rem"}}>
+                    <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/A464/production/_121148024_pattinson_getty.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href="/takingtonowhere" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
             </div>
         )
     }
